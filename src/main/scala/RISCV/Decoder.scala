169411e92a462fe74b34a1a01bc630fee218307d
outercloudstudio <outercloudstudio@gmail.com>
2025-11-06 13:20:55 -0500

    Add instruction formats to decoder

--- src/main/scala/RISCV/Decoder.scala
+++ src/main/scala/RISCV/Decoder.scala
@@ -3,10 +3,21 @@ package RISCV
 import chisel3._
 import chisel3.util._
 import _root_.circt.stage.ChiselStage
+import upickle.default
+
+object InstructionFormat extends ChiselEnum {
+  val R, I, S, B, U, J = Value
+}
+
+object Opcode extends ChiselEnum {
+  val ADDI  = Value(0b0010011.U)
+  val AUIPC = Value(0b0010111.U)
+  val LUI   = Value(0b0110111.U)
+}
 
 /**
-	* @param width Bit width (default: 32 bits)
-	*/
+* @param width Bit width (default: 32 bits)
+*/
 class Decoder(val width: Int = 32) extends Module {
 	val io = IO(new Bundle {
 		val instruction = Input(UInt(32.W));
@@ -21,9 +32,45 @@ class Decoder(val width: Int = 32) extends Module {
 	io.rs2 := io.instruction(24, 20);
 	io.rd := io.instruction(11, 7);
 
+	val format = Wire(InstructionFormat());
+	format := InstructionFormat.R;
+
+	val opcode = io.instruction(6,0);
+
+	switch(opcode) {
+		is(Opcode.LUI.asUInt) { format := InstructionFormat.U; }
+		is(Opcode.AUIPC.asUInt) { format := InstructionFormat.U; }
+		is(Opcode.ADDI.asUInt) { format := InstructionFormat.I; }
+	}
+
 	io.operation := 0.U;
-	
 	io.immediate := 0.U;
+
+	switch(format){
+		is(InstructionFormat.R) {
+			io.operation := io.instruction(31,25) ## io.instruction(14,12) ## io.instruction(6,0);
+		}
+		is(InstructionFormat.I) {
+			io.operation := io.instruction(14,12) ## io.instruction(6,0);
+			io.immediate := io.instruction(31,31) ## 0.U(20.W) ## io.instruction(30,20);
+		}
+		is(InstructionFormat.S) {
+			io.operation := io.instruction(14,12) ## io.instruction(6,0);
+			io.immediate := io.instruction(31,31) ## 0.U(20.W) ## io.instruction(31,25) ## io.instruction(11,7);
+		}
+		is(InstructionFormat.B) {
+			io.operation := io.instruction(14,12) ## io.instruction(6,0);
+			io.immediate := io.instruction(31,31) ## 0.U(19.W) ## io.instruction(7,7) ## io.instruction(31,25) ## io.instruction(11,8) ## 0.U(1.W);
+		}
+		is(InstructionFormat.U) {
+			io.operation := io.instruction(6,0);
+			io.immediate := io.instruction(31,12) ## 0.U(12.W);
+		}
+		is(InstructionFormat.J) {
+			io.operation := io.instruction(6,0);
+			io.immediate := io.instruction(31,31) ## 0.U(11.W) ## io.instruction(19,12) ## io.instruction(20,20) ## io.instruction(30,21) ## 0.U(1.W);
+		}
+	}
 }
 
 object Decoder extends App {
--- src/test/scala/RISCV/DecoderTest.scala
+++ src/test/scala/RISCV/DecoderTest.scala
@@ -9,7 +9,10 @@ import org.scalatest.matchers.must.Matchers
 class DecoderTest extends AnyFreeSpec with Matchers with ChiselSim {
   "Decoder should pass tests" in {
     simulate(new Decoder()) { dut =>
-      dut.io.instruction.poke(0.U);
+      dut.io.instruction.poke(0b00000000000000000101_00001_0110111.U);
+      dut.io.operation.expect(0b0000000_000_0110111.U);
+      dut.io.rd.expect(0b00001.U);
+      dut.io.immediate.expect(0b00000000000000000101_000000000000.U);
     }
   }
 }
