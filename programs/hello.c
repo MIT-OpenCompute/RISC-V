864d67e4973f53f92bed6898c35a548c61a442fa
Outer Cloud <outercloudstudio@gmail.com>
2026-03-08 03:06:05 -0400

    update program

--- programs/hello.c
+++ programs/hello.c
@@ -1,22 +1,21 @@
 __attribute__((naked)) void _start(void) {
-    __asm__ volatile (
-        "li sp, 0x8000\n"   // top of 32KB RAM
+    __asm__ volatile(
+        "li sp, 0x8000\n"  // top of 32KB RAM
         "call main\n"
-        "loop: j loop\n"
-    );
+        "loop: j loop\n");
 }
 
 int main() {
-    volatile unsigned char* mem = (volatile unsigned char*)0x4000;
+    volatile unsigned char* mem = (volatile unsigned char*)0x0;
     int counter = 0;
 
     while (1) {
         counter++;
-        mem[counter] = counter;  // sb x1, 0(x2) — store counter byte
+        mem[0x4000 * counter] = counter;
 
         // busy-wait pause loop (~2000 iterations)
-        for (int i = 0; i < 2000; i++) {
-            __asm__("nop");  // prevent compiler from optimizing away
-        }
+        // for (int i = 0; i < 2000; i++) {
+        //     __asm__("nop");  // prevent compiler from optimizing away
+        // }
     }
 }
--- programs/hello.s
+++ programs/hello.s
@@ -5,6 +5,20 @@
 	.attribute stack_align, 16
 	.text
 	.align	2
+	.globl	_start
+	.type	_start, @function
+_start:
+ #APP
+# 2 "./programs/hello.c" 1
+	li sp, 0x8000
+call main
+loop: j loop
+
+# 0 "" 2
+ #NO_APP
+	nop
+	.size	_start, .-_start
+	.align	2
 	.globl	main
 	.type	main, @function
 main:
@@ -12,35 +26,21 @@ main:
 	sw	ra,28(sp)
 	sw	s0,24(sp)
 	addi	s0,sp,32
-	li	a5,16384
-	sw	a5,-28(s0)
+	sw	zero,-24(s0)
 	sw	zero,-20(s0)
-.L4:
+.L3:
 	lw	a5,-20(s0)
 	addi	a5,a5,1
 	sw	a5,-20(s0)
 	lw	a5,-20(s0)
-	lw	a4,-28(s0)
-	add	a5,a4,a5
+	slli	a5,a5,14
+	mv	a4,a5
+	lw	a5,-24(s0)
+	add	a5,a5,a4
 	lw	a4,-20(s0)
 	andi	a4,a4,0xff
 	sb	a4,0(a5)
-	sw	zero,-24(s0)
-	j	.L2
-.L3:
- #APP
-# 11 "./programs/hello.c" 1
-	nop
-# 0 "" 2
- #NO_APP
-	lw	a5,-24(s0)
-	addi	a5,a5,1
-	sw	a5,-24(s0)
-.L2:
-	lw	a4,-24(s0)
-	li	a5,1999
-	ble	a4,a5,.L3
-	j	.L4
+	j	.L3
 	.size	main, .-main
 	.ident	"GCC: (xPack GNU RISC-V Embedded GCC x86_64) 15.2.0"
 	.section	.note.GNU-stack,"",@progbits
