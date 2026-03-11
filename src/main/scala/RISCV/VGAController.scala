782fc81cabcf89454f9cb7608fba0008191bfbe4
ag <acgo8888@gmail.com>
2026-03-11 00:58:14 -0400

    test 1

--- src/main/scala/RISCV/VGAController.scala
+++ src/main/scala/RISCV/VGAController.scala
@@ -21,58 +21,67 @@ class VGAController extends Module {
         val address = Input(UInt(32.W))
         val write = Input(Bool())
         val write_value = Input(UInt(8.W))
+        val read_clk = Input(Clock())
 
         val hsync = Output(Bool())
         val vsync = Output(Bool())
         val rgb = Output(UInt(12.W))
         val blanking = Output(Bool())
+
     })
 
     val memory = SyncReadMem(320 * 240, UInt(8.W))
 
-    val hCount = RegInit(0.U(10.W))
-    val vCount = RegInit(0.U(10.W))
+
 
     when(io.write) {
         memory.write(io.address, io.write_value)
     }
 
-    when(hCount === (H_TOTAL - 1).U) {
-        hCount := 0.U
+    withClock(io.read_clk) {
+        val hCount = RegInit(0.U(10.W))
+        val vCount = RegInit(0.U(10.W))
 
-        when(vCount === (V_TOTAL - 1).U) {
-            vCount := 0.U
+
+        when(hCount === (H_TOTAL - 1).U) {
+            hCount := 0.U
+
+            when(vCount === (V_TOTAL - 1).U) {
+                vCount := 0.U
+            }.otherwise {
+                vCount := vCount + 1.U
+            }
         }.otherwise {
-            vCount := vCount + 1.U
+            hCount := hCount + 1.U
         }
-    }.otherwise {
-        hCount := hCount + 1.U
+
+        val hSyncStart = (H_VISIBLE + H_FRONT).U
+        val hSyncEnd = (H_VISIBLE + H_FRONT + H_SYNC).U
+        val vSyncStart = (V_VISIBLE + V_FRONT).U
+        val vSyncEnd = (V_VISIBLE + V_FRONT + V_SYNC).U
+
+        io.hsync := !(hCount >= hSyncStart && hCount < hSyncEnd)
+        io.vsync := !(vCount >= vSyncStart && vCount < vSyncEnd)
+
+        val hActive = hCount < H_VISIBLE.U
+        val vActive = vCount < V_VISIBLE.U
+        val active = hActive && vActive
+
+        io.blanking := !active
+
+        val read_address = WireInit(0.U(32.W))
+
+        when(active) {
+            read_address := vCount / 2.U * 320.U + hCount / 2.U + 1.U
+        }.otherwise {
+            read_address := (vCount + 1.U) / 2.U * 320.U
+        }
+
+        val color = memory.read(read_address, true.B)
+        val pixel = color(7, 5) ## color(5) ## color(4, 2) ## color(2) ## color(1, 0) ## color(0) ## color(0)
+
+        io.rgb := Mux(active, pixel, 0.U)
+
     }
-
-    val hSyncStart = (H_VISIBLE + H_FRONT).U
-    val hSyncEnd = (H_VISIBLE + H_FRONT + H_SYNC).U
-    val vSyncStart = (V_VISIBLE + V_FRONT).U
-    val vSyncEnd = (V_VISIBLE + V_FRONT + V_SYNC).U
-
-    io.hsync := !(hCount >= hSyncStart && hCount < hSyncEnd)
-    io.vsync := !(vCount >= vSyncStart && vCount < vSyncEnd)
-
-    val hActive = hCount < H_VISIBLE.U
-    val vActive = vCount < V_VISIBLE.U
-    val active = hActive && vActive
-
-    io.blanking := !active
-
-    val read_address = WireInit(0.U(32.W))
-
-    when(active) {
-        read_address := vCount / 2.U * 320.U + hCount / 2.U + 1.U
-    }.otherwise {
-        read_address := (vCount + 1.U) / 2.U * 320.U
-    }
-
-    val color = memory.read(read_address, true.B)
-    val pixel = color(7, 5) ## color(5) ## color(4, 2) ## color(2) ## color(1, 0) ## color(0) ## color(0)
-
-    io.rgb := Mux(active, pixel, 0.U)
+  
 }
