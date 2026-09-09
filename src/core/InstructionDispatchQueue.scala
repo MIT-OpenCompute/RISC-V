2af381a9d91eb9f2d38ab03b6224163c65a80301
Liam Hanrahan <outercloudstudio@gmail.com>
2026-09-09 01:35:24 -0400

    fix syntax error

--- src/core/InstructionDispatchQueue.scala
+++ src/core/InstructionDispatchQueue.scala
@@ -49,17 +49,21 @@ class InstructionDispatchQueue() extends Module {
         val index = Wire(UInt(3.W))
         val valid = Wire(Bool())
 
-        if(high == low) {
+        if (high == low) {
             index := low.U
             valid := queue(low.U).valid
-				&& queue(low.U).instruction.rs1_dependence_counter === 0.U
-				&& queue(low.U).instruction.rs2_dependence_counter === 0.U
-				&& queue(low.U).instruction.rd_dependence_counter === 0.U
-				&& (
-					(io.alu_ready && queue(low.U).instruction.pe_type === PeType.Alu)
-					|| (io.lsu_ready && queue(low.U).instruction.pe_type === PeType.Lsu && queue(low.U).instruction.reorder_pointer === io.reorder_buffer_tail)
-					|| (io.jump_unit_ready && queue(low.U).instruction.pe_type === PeType.JumpUnit && queue(low.U).instruction.reorder_pointer === io.reorder_buffer_tail
-				)
+            && queue (low.U).instruction.rs1_dependence_counter === 0.U
+            && queue (low.U).instruction.rs2_dependence_counter === 0.U
+            && queue (low.U).instruction.rd_dependence_counter === 0.U
+            &&(
+              (io.alu_ready && queue(low.U).instruction.pe_type === PeType.Alu)
+                  || (io.lsu_ready && queue(low.U).instruction.pe_type === PeType.Lsu && queue(
+                    low.U
+                  ).instruction.reorder_pointer === io.reorder_buffer_tail)
+                  || (io.jump_unit_ready && queue(low.U).instruction.pe_type === PeType.JumpUnit && queue(
+                    low.U
+                  ).instruction.reorder_pointer === io.reorder_buffer_tail)
+            )
         } else {
             val mid = low + (high - low) / 2
             val lowResult = compare_entry(low, mid)
@@ -123,27 +127,27 @@ class InstructionDispatchQueue() extends Module {
             queue(n.U).valid := false.B
 
             when(
-                io.broadcast_free_valid && queue(n.U).instruction.rs1 === io.broadcast_free_register && queue(
+              io.broadcast_free_valid && queue(n.U).instruction.rs1 === io.broadcast_free_register && queue(
                 n.U
-                ).instruction.rs1_dependence_counter > 0.U
+              ).instruction.rs1_dependence_counter > 0.U
             ) {
                 queue((n - 1).U).instruction.rs1_value := io.broadcast_free_value
                 queue((n - 1).U).instruction.rs1_dependence_counter := queue(n.U).instruction.rs1_dependence_counter - 1.U
             }
 
             when(
-                io.broadcast_free_valid && queue(n.U).instruction.rs2 === io.broadcast_free_register && queue(
+              io.broadcast_free_valid && queue(n.U).instruction.rs2 === io.broadcast_free_register && queue(
                 n.U
-                ).instruction.rs2_dependence_counter > 0.U
+              ).instruction.rs2_dependence_counter > 0.U
             ) {
                 queue((n - 1).U).instruction.rs2_value := io.broadcast_free_value
                 queue((n - 1).U).instruction.rs2_dependence_counter := queue(n.U).instruction.rs2_dependence_counter - 1.U
             }
 
             when(
-                io.broadcast_free_valid && queue(n.U).instruction.rd === io.broadcast_free_register && queue(
+              io.broadcast_free_valid && queue(n.U).instruction.rd === io.broadcast_free_register && queue(
                 n.U
-                ).instruction.rd_dependence_counter > 0.U
+              ).instruction.rd_dependence_counter > 0.U
             ) {
                 queue((n - 1).U).instruction.rd_dependence_counter := queue(n.U).instruction.rd_dependence_counter - 1.U
             }
@@ -205,19 +209,19 @@ class InstructionDispatchQueue() extends Module {
         for (n <- 0 to 7) {
             when(queue(n.U).valid) {
                 printf(
-                "%d -> opcode: %b rp: %d ip: %d rd: %d %d rs1: %d %d %d rs2: %d %d %d \n",
-                n.U,
-                queue(n.U).instruction.opcode,
-                queue(n.U).instruction.reorder_pointer,
-                queue(n.U).instruction.instruction_pointer,
-                queue(n.U).instruction.rd,
-                queue(n.U).instruction.rd_dependence_counter,
-                queue(n.U).instruction.rs1,
-                queue(n.U).instruction.rs1_value,
-                queue(n.U).instruction.rs1_dependence_counter,
-                queue(n.U).instruction.rs2,
-                queue(n.U).instruction.rs2_value,
-                queue(n.U).instruction.rs2_dependence_counter
+                  "%d -> opcode: %b rp: %d ip: %d rd: %d %d rs1: %d %d %d rs2: %d %d %d \n",
+                  n.U,
+                  queue(n.U).instruction.opcode,
+                  queue(n.U).instruction.reorder_pointer,
+                  queue(n.U).instruction.instruction_pointer,
+                  queue(n.U).instruction.rd,
+                  queue(n.U).instruction.rd_dependence_counter,
+                  queue(n.U).instruction.rs1,
+                  queue(n.U).instruction.rs1_value,
+                  queue(n.U).instruction.rs1_dependence_counter,
+                  queue(n.U).instruction.rs2,
+                  queue(n.U).instruction.rs2_value,
+                  queue(n.U).instruction.rs2_dependence_counter
                 )
             }.otherwise {
                 printf("%d -> \n", n.U)
@@ -234,36 +238,36 @@ class InstructionDispatchQueue() extends Module {
 
         when(io.jump_unit_out_valid) {
             printf(
-                "Dispatching to jump unit! op: %b rp: %d ip: %d\n",
-                io.jump_unit_out.opcode,
-                io.jump_unit_out.reorder_pointer,
-                io.jump_unit_out.instruction_pointer
+              "Dispatching to jump unit! op: %b rp: %d ip: %d\n",
+              io.jump_unit_out.opcode,
+              io.jump_unit_out.reorder_pointer,
+              io.jump_unit_out.instruction_pointer
             )
         }
 
         when(io.lsu_out_valid) {
             printf(
-                "Dispatching to lsu! op: %b rp: %d ip: %d\n",
-                io.lsu_out.opcode,
-                io.lsu_out.reorder_pointer,
-                io.lsu_out.instruction_pointer
+              "Dispatching to lsu! op: %b rp: %d ip: %d\n",
+              io.lsu_out.opcode,
+              io.lsu_out.reorder_pointer,
+              io.lsu_out.instruction_pointer
             )
         }
 
         when(io.alu_out_valid) {
             printf(
-                "Dispatching to alu! op: %b rp: %d ip: %d\n",
-                io.alu_out.opcode,
-                io.alu_out.reorder_pointer,
-                io.alu_out.instruction_pointer
+              "Dispatching to alu! op: %b rp: %d ip: %d\n",
+              io.alu_out.opcode,
+              io.alu_out.reorder_pointer,
+              io.alu_out.instruction_pointer
             )
         }
 
         when(io.broadcast_free_valid) {
             printf(
-                "Register freed! %d %d\n",
-                io.broadcast_free_register,
-                io.broadcast_free_value
+              "Register freed! %d %d\n",
+              io.broadcast_free_register,
+              io.broadcast_free_value
             )
         }
     }
